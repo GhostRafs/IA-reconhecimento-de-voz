71898727aebb568a59854d824f4b61afa69ac4ea
GhostRafs <rafinhapsampaio@hotmail.com>
2024-10-30 19:43:22 -0300

    remoção arquivos inúteis

--- config.py
+++
@@ -1,8 +0,0 @@
-DATASET_PATH = '/content/ShEMO'
-HUBERT = "facebook/hubert-base-ls960"
-DISTIL_HUBERT = "ntu-spml/distilhubert"
-LR = 1e-5
-EPOCHS = 20
-WEIGHT_DECAY = 0.005
-SCH_GAMMA = 0.1
-SCH_STEP = 10
